65ec25916749409076c14ea00be8ed0ed0d270a0
Tadres <nicolas.tadres@edu.esiee.fr>
2023-05-01 18:24:49 +0200

    Mise en place de l'authentification

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import jwt_utils
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,15 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def PostLogin():
+	payload = request.get_json()
+	if payload['password'] == "flask2023":
+		token = jwt_utils.build_token()
+		return {"token":token}, 200
+	return {"error": "Unauthorized"}, 401
+
 
 if __name__ == "__main__":
     app.run()
